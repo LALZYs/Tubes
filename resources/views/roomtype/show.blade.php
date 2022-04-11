7adae782b70882efd44034caaf737f727159b0ff
unknown <mbilalrafif@gmail.com>
2022-04-11 16:18:36 +0700

    update terakhir

--- resources/views/roomtype/show.blade.php
+++ resources/views/roomtype/show.blade.php
@@ -6,7 +6,7 @@
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <a href="{{url('admin/roomtype')}}" class="float-right btn btn-success btn-sm">View All</a>
+                <a href="{{url('admin/roomtype')}}" class="float-right btn btn-success btn-sm">Tampilkan Semua</a>
             </div>
             <div class="card-body">
                 <div class="table-responsive">                    
@@ -34,11 +34,6 @@
                         <tr>
                             <th>Tourguide</th>
                             <td>{{$data->tourguide}}</td>
-                        </tr>
-                        <tr>
-                            <td colspan="2">
-                                <input type="submit" class="btn btn-primary" />
-                            </td>
                         </tr>
                     </table>
                 </div>
