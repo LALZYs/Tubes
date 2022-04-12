dc63d6d1a2df3cde106182c6061a4168638bc64d
unknown <mbilalrafif@gmail.com>
2022-04-12 15:47:29 +0700

    update edit.blade.php

--- resources/views/tempatpariwisata/edit.blade.php
+++ resources/views/tempatpariwisata/edit.blade.php
@@ -7,7 +7,7 @@
         <div class="card shadow mb-4">
             <div class="card-header py-3">
                 <h6 class="m-0 font-weight-bold text-primary">Edit Tempat Pariwisata
-                    <a href="{{url('admin/tempatpariwisata')}}" class="float-right btn btn-success btn-sm">View All</a>
+                    <a href="{{url('admin/tempatpariwisata')}}" class="float-right btn btn-success btn-sm">Tampilkan Semua</a>
                 </h6>
             </div>
             <div class="card-body">
