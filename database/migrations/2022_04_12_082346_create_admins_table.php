c0cf2188d005f38fcb988ce4a0c5c4bb3c7bc4c2
unknown <mbilalrafif@gmail.com>
2022-04-12 15:41:25 +0700

    LAST

--- database/migrations/2022_04_12_082346_create_admins_table.php
+++ database/migrations/2022_04_12_082346_create_admins_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('username');
             $table->string('password');
+            $table->string('email');
+            $table->string('phone_number');
             $table->string('unique_code');
-            $table->string('asal_kota');
-            $table->string('role');
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user');
+       
     }
 };
