6e50c7885f1da0b6cd4d04cf28e85e1d0421890c
unknown <mbilalrafif@gmail.com>
2022-04-21 19:24:08 +0700

    update crud admin

--- resources/views/tempatpariwisata/show.blade.php
+++ resources/views/tempatpariwisata/show.blade.php
@@ -6,7 +6,9 @@
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">{{ $data->nama_tempat }}
                 <a href="{{url('admin/tempatpariwisata')}}" class="float-right btn btn-success btn-sm">Tampilkan Semua</a>
+                </h6>
             </div>
             <div class="card-body">
                 <div class="table-responsive">
