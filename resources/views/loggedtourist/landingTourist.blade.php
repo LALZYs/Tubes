3dbf68949af5a18ca6af3e38ce76024dc0a4c14c
Kheedos <naufaladitya02@gmail.com>
2022-06-22 00:37:50 +0700

    update landingtourist

--- resources/views/loggedtourist/landingTourist.blade.php
+++ resources/views/loggedtourist/landingTourist.blade.php
@@ -30,10 +30,10 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        
+
         <ul style="background-color: #2c2534;" class="navbar-nav  sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            
+
             <!-- Sidebar - Brand -->
             <div class="sidebar-header d-flex justify-content-left align-items-center px-3 py-4">
                 <img
@@ -43,7 +43,7 @@
                     alt="">
                 <div class="ms-2">
                 <h5 class="fs-6 mb-0">
-                    <a class="text-decoration-none" href="#">{{Session::get('username');}}</a>
+                    <a class="text-decoration-none" href="{{route('loggedrist')}}">{{Session::get('username');}}</a>
                 </h5>
                 </div>
             </div>
@@ -71,9 +71,9 @@
                 </div>
             </li>
 
-        
 
-         
+
+
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
@@ -93,22 +93,22 @@
             <div id="content">
 
                 <!-- Topbar -->
-                <nav style="background-color: #000000;" class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                         <i class="fa fa-bars"></i>
                     </button>
 
-                   
+
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
 
-                        
+
                         <!-- Nav Item - User Information -->
                         <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                            <a class="nav-link dropdown-toggle" href="{{route('loggedrist')}}" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{Session::get('username');}}</span>
                                 <img class="img-profile rounded-circle" src="/img/undraw_profile.svg">
@@ -133,8 +133,10 @@
                 </nav>
                 <!-- End of Topbar -->
 
-                <div id="parallax-world-of-ugg">
-  
+                @yield('content')
+
+                                <div id="parallax-world-of-ugg">
+                
                 <section>
                     <div class="title">
                     <h3>Let's do some</h3>
@@ -150,9 +152,9 @@
                 
                 <section>
                     <div class="block">
-                    <p><span class="first-character sc">I</span>n 1978, Brian Smith landed in Southern California with a bag of sheepskin boots and hope. He fell in love with the sheepskin experience and was convinced the world would one day share this love. The beaches of Southern California had long been an epicenter of a relaxed, casual lifestyle, a lifestyle that Brian felt was a perfect fit for his brand. So he founded the UGG brand, began selling his sheepskin boots and they were an immediate sensation. By the mid 1980's, the UGG brand became a symbol of relaxed southern California culture, gaining momentum through surf shops and other shops up and down the coast of California, from San Diego to Santa Cruz. UGG boots reached beyond the beach, popping up in big cities and small towns all over, and in every level of society. Girls wore their surfer boyfriend's pair of UGG boots like a letterman jacket. When winter came along, UGG boots were in ski shops and were seen in lodges from Mammoth to Aspen.</p>
+                    <p><span class="first-character sc">B</span>Bali adalah pulau yang sangat indah dengan panjang garis pantai sekitar 633,35 km. Di Pulau Bali terdapat gunung berapi, sungai-sungai, dan danau. Keberadaan gunung berapi memberikan kesuburan tanah untuk usaha pertanian. Gunung berapi di Pulau Bali adalah Gunung Batur dan Gunung Agung.</p>
                     <p class="line-break margin-top-10"></p>
-                    <p class="margin-top-10">The UGG brand began to symbolize those who embraced sport and a relaxed, active lifestyle. More than that, an emotional connection and a true feeling of love began to grow for UGG boots, just as Brian had envisioned. People didn't just like wearing UGG boots, they fell in love with them and literally could not take them off. By the end of the 90's, celebrities and those in the fashion world took notice of the UGG brand. A cultural shift occurred as well - people were embracing, and feeling empowered, by living a more casual lifestyle and UGG became one of the symbols of this lifestyle. By 2000, a love that began on the beaches had become an icon of casual style. It was at this time that the love for UGG grew in the east, over the Rockies and in Chicago. In 2000, UGG Sheepskin boots were first featured on Oprah's Favorite Things® and Oprah emphatically declared that she "LOOOOOVES her UGG boots." From that point on, the world began to notice.</p>
+                    <p class="margin-top-10">Bali memang memiliki banyak hal yang ditonjolkan seperti keindahan alamnya mulai dari pantai, sawah terasering, pegunungan dan juga danaunya, merupakan objek wisata unggulan yang selalu diminati oleh wisatawan, apalagi sekarang warga juga lebih kreatif untuk menciptakan tempat-tempat wisata baru yang tampil kekinian dan instagramable, dan sangat diminati oleh mereka yang suka hunting foto maupun yang gemar foto selfie. Selain itu Bali memiliki berbagai budaya seni dan tradisi unik yang menjadi daya tarik sendiri bagi wisatawan. Bali memiliki destinasi parawisata paling menonjol yaitu pantai. Bali memiliki pantai - pantai yang memiliki keindahan tiada tara, sehingga banyak tourist dalam negeri dan tourist luar negeri yang berdatangan ke Bali untuk menikmati keindahan alamnya. </p>
                     </div>
                 </section>
                 
@@ -164,9 +166,9 @@
                 
                 <section>
                     <div class="block">
-                    <p><span class="first-character ny">B</span>reaking into the New York fashion world is no easy task. But by the early 2000's, UGG Australia began to take it by storm. The evolution of UGG from a brand that made sheepskin boots, slippers, clogs and sandals for an active, outdoor lifestyle to a brand that was now being touted as a symbol of a stylish, casual and luxurious lifestyle was swift. Much of this was due to a brand repositioning effort that transformed UGG into a high-end luxury footwear maker. As a fashion brand, UGG advertisements now graced the pages of Vogue Magazine as well as other fashion books. In the mid 2000's, the desire for premium casual fashion was popping up all over the world and UGG was now perfectly aligned with this movement.</p>
+                    <p><span class="first-character ny">J</span>Daerah Khusus Ibukota Jakarta (DKI Jakarta) adalah ibu kota negara dan kota terbesar di Indonesia. Jakarta merupakan satu-satunya kota di Indonesia yang memiliki status setingkat provinsi. Jakarta terletak di pesisir bagian barat laut Pulau Jawa.</p>
                     <p class="line-break margin-top-10"></p>
-                    <p class="margin-top-10">Fueled by celebrities from coast to coast wearing UGG boots and slippers on their downtime, an entirely new era of fashion was carved out. As a result, the desire and love for UGG increased as people wanted to go deeper into this relaxed UGG experience. UGG began offering numerous color and style variations on their sheepskin boots and slippers. Cold weather boots for women and men and leather casuals were added with great success. What started as a niche item, UGG sheepskin boots were now a must-have staple in everyone's wardrobe. More UGG collections followed, showcasing everything from knit boots to sneakers to wedges, all the while maintaining that luxurious feel UGG is known for all over the world. UGG products were now seen on runways and in fashion shoots from coast to coast. Before long, the love spread even further.</p>
+                    <p class="margin-top-10">Jakarta merupakan salah satu kota di Indonesia yang menjadi destinasi favorit bagi wisatawan domestik maupun mancanegara. Banyak sekali pilihan tempat wisata di Jakarta yang bisa dituju saat liburan, khususnya wisata sejarah. Berbagai objek wisata menarik seperti wisata budaya, wisata kuliner, wisata religi, hingga wisata alam pun bisa ditemui di Kota Metropolitan ini.</p>
                     </div>
                 </section>
                 
@@ -178,32 +180,34 @@
                 
                 <section>
                     <div class="block">
-                    <p><span class="first-character atw">W</span>hen the New York fashion community notices your brand, the world soon follows. The widespread love for UGG extended to Europe in the mid-2000's along with the stylish casual movement and demand for premium casual fashion. UGG boots and shoes were now seen walking the streets of London, Paris and Amsterdam with regularity. To meet the rising demand from new fans, UGG opened flagship stores in the UK and an additional location in Moscow. As the love spread farther East, concept stores were opened in Beijing, Shanghai and Tokyo. UGG Australia is now an international brand that is loved by all. This love is a result of a magical combination of the amazing functional benefits of sheepskin and the heightened emotional feeling you get when you slip them on your feet. In short, you just feel better all over when you wear UGG boots, slippers, and shoes.</p>
+                    <p><span class="first-character atw">B</span>Bandung adalah Ibukota provinsi dari Jawa Barat. Kota ini terkenal dengan hawanya yang sejuk dan dikelilingi oleh pegunungan. Pada zaman dahulu Bandung dijuluki sebagai Parijs van Java (Paris dari Jawa).</p>
                     <p class="line-break margin-top-10"></p>
-                    <p class="margin-top-10">In 2011, UGG will go back to its roots and focus on bringing the active men that brought the brand to life back with new styles allowing them to love the brand again as well. Partnering with Super Bowl champion and NFL MVP Tom Brady, UGG will invite even more men to feel the love the rest of the world knows so well. UGG will also step into the world of high fashion with UGG Collection. The UGG Collection fuses the timeless craft of Italian shoemaking with the reliable magic of sheepskin, bringing the luxurious feel of UGG to high end fashion. As the love for UGG continues to spread across the world, we have continued to offer new and unexpected ways to experience the brand. The UGG journey continues on and the love for UGG continues to spread.</p>
+                    <p class="margin-top-10">Kota Bandung adalah salah satu kota terbesar yang ada di Indonesia. Kota ini juga dijuluki sebagai “Paris Van Java” atau kota Paris-nya Jawa. Karena semua hal ada di Bandung. Kota ini juga memiliki tata kota yang apik dan fasilitas yang memadai untuk para pelancong, baik yang berasal dari dalam negeri maupun wisatawan luar negeri.
+
+Kota ini tidak hanya dijuluki sebagai kota metropolitan, tetapi juga kota dengan banyak tempat wisata yang menyenangkan. Daerahnya juga menyimpan berbagai keindahan alam yang sangat luar biasa. Ada banyak sekali tempat wisata di bandung, seperti taman kota, tempat peninggalan sejarah, wisata alam, danau dan lain nya.</p>
                     </div>
                 </section>
                     
                 </div>
                 
-            </div>
+                </div>
 
-        </div>
-        <!-- End of Content Wrapper -->
+                </div>
+                <!-- End of Content Wrapper -->
 
-    </div>
-    <!-- End of Page Wrapper -->
+                </div>
+                <!-- End of Page Wrapper -->
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
+                <!-- Scroll to Top Button-->
+                <a class="scroll-to-top rounded" href="#page-top">
+                <i class="fas fa-angle-up"></i>
+                </a>
 
-    <!-- Logout -->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
+                <!-- Logout -->
+                <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Apakah anda yakin ingin logout ?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
@@ -218,23 +222,23 @@
                         <button type="submit" class="btn btn-primary">Logout</button>
                     </form>
                 </div>
-            </div>
-        </div>
-    </div>
+                </div>
+                </div>
+                </div>
 
-    <!-- Bootstrap core JavaScript-->
+                <!-- Bootstrap core JavaScript-->
 
-    <script src="/vendor/jquery/jquery.min.js"></script>
-    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+                <script src="/vendor/jquery/jquery.min.js"></script>
+                <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
+                <!-- Core plugin JavaScript-->
+                <script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="/js/sb-admin-2.min.js"></script>
+                <!-- Custom scripts for all pages-->
+                <script src="/js/sb-admin-2.min.js"></script>
 
-    @yield('scripts')
+                @yield('scripts')
 
-</body>
+                </body>
 
-</html>
+                </html>
